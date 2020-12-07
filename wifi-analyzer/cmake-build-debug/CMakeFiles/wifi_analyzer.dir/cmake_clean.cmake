784005fedac9d672bea5ea87d20e225896403120
edionu97 <onu_edy@yahoo.com>
2020-12-07 19:49:48 +0200

    Added hello world project

---
+++ wifi-analyzer/cmake-build-debug/CMakeFiles/wifi_analyzer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wifi_analyzer.dir/main.cpp.o"
+  "wifi_analyzer"
+  "wifi_analyzer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wifi_analyzer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
