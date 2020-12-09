d777148282ae5b25172f02dcc10c5155e5da0756
edionu97 <onu_edy@yahoo.com>
2020-12-09 19:13:17 +0200

    added more information extraction

--- rdsos/main.cpp
+++ rdsos/main.cpp
@@ -1,9 +1,10 @@
 #include <cstdio>
 #include <iwlib.h>
 #include <string>
+#include <cstring>
 
-#define clear() printf("\033[H\033[J")
 
+//iwlist interface scan
 int main()
 {
     wireless_scan_head head;
@@ -14,7 +15,7 @@ int main()
     /* Open socket to kernel */
     sock = iw_sockets_open();
 
-    while(true)
+    while (true)
     {
         std::string wlan_name{"wlp0s20f3"};
 
@@ -36,9 +37,35 @@ int main()
         result = head.result;
         while (nullptr != result)
         {
-            printf("%s\n", result->b.essid);
+            printf("\n");
+
+            //ssid
+            printf("SSID: %s\n", result->b.essid);
+
+            //mac address
+            printf("MAC access point: ");
+            for (int i = 0; i < strlen(result->ap_addr.sa_data); ++i, printf("."))
+            {
+                printf("%02X", (unsigned char) result->ap_addr.sa_data[i]);
+            }
+
+            printf("\n");
+            //get the quality percentage
+            printf("Quality: %d/%d\n", result->stats.qual.qual, range.max_qual.qual);
+
+            //get the frequency (get the shortest representation)
+            printf("Frequency: %.10g Ghz\n", result->b.freq / 1000000000);
+
+            //get the channel
+            printf("Channel: %d\n", iw_freq_to_channel(result->b.freq, &range));
+
+            //get the signal level
+            printf("Signal level: %d dBm (higher is better)\n", (int8_t) result->stats.qual.level);
+
             result = result->next;
         }
+
+        return 0;
 
         sleep(1);
     }
